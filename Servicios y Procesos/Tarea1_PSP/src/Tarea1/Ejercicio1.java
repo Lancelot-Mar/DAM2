a68afc5e2bbb87d9ff046a98426e6946a44e5e92
Leonardo Marescutti <155202752+Lancelot-Mar@users.noreply.github.com>
2025-11-25 14:02:09 +0100

    psp tarea fran

--- Servicios y Procesos/Tarea1_PSP/src/Tarea1/Ejercicio1.java
+++ Servicios y Procesos/Tarea1_PSP/src/Tarea1/Ejercicio1.java
@@ -1,5 +1,36 @@
 package Tarea1;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Ejercicio1 {
 
+    public static void main(String[] args) {
+
+        try {
+
+            FileOutputStream fos = new FileOutputStream("salida.txt", true);
+            Jugador.setFileOutputStream(fos);
+
+            Jugador cifras = new Jugador("cifras");
+            Jugador letras = new Jugador("letras");
+
+            cifras.start();
+            letras.start();
+
+            synchronized (Jugador.class) {
+                try {
+                    Jugador.class.wait();  // wait al ganador
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            fos.close();
+            System.out.println("Juego finalizado. Revisa salida.txt");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- Servicios y Procesos/Tarea1_PSP/src/Tarea1/Jugador.java
+++ Servicios y Procesos/Tarea1_PSP/src/Tarea1/Jugador.java
@@ -1,5 +1,60 @@
 package Tarea1;
 
-public class Jugador {
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
 
+public class Jugador extends Thread {
+
+    private static boolean terminado = false;
+    private static FileOutputStream fosSalida;
+
+    private final String tipo;
+    private final Random random = new Random();
+
+    private static final char[] arrayLetras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
+    public Jugador(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public static void setFileOutputStream(FileOutputStream fos) {
+        fosSalida = fos;
+    }
+
+    private char letraRandom() {
+        return arrayLetras[random.nextInt(arrayLetras.length)];
+    }
+
+    private char cifraRandom() {
+        return (char) (random.nextInt(10) + '0');
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 20; i++) {
+
+            synchronized (Jugador.class) {   // <-- monitor único
+
+                if (terminado) return;
+
+                try {
+                    char c = (tipo.equals("cifras")) ? cifraRandom() : letraRandom();
+                    fosSalida.write(c);
+
+                    Thread.sleep(random.nextInt(1000));
+
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                if (i == 19 && !terminado) {
+                    terminado = true;
+                    System.out.println("GANADOR: " + tipo.toUpperCase());
+                    Jugador.class.notifyAll();
+                }
+            }
+        }
+    }
 }
